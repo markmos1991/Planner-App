90afaa633e43f9d58b79be67c94e43df46c06141
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-01-20 21:22:46 +0000

    for loop created to add ttimeblocks for working hours

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,37 @@
 
 
 
+var root =  $('#root');
 
-// submit in modal
-function handleProjectFormSubmit(event) {
-    event.preventDefault();
-
-    var projectName = projectNameInputEl.val().trim();
+// Time in Jumbo
+var today = moment();
+$("#currentDay").text(today.format("D MMM YYYY"));
 
 
-    printProjectdata();
+
+// create 9 time blocks for the working day using a for loop
+function createTimeBlocks() {
+
+for (var i = 9; i < 18; i++) {
+  var timeBlock = $("<div>");
+  root.append(timeBlock);
+  timeBlock.addClass("time-block");
+  
+// create a div within the timeBlock for the hour
+  var hourDiv = $("<div>");
+  hourDiv.addClass("hour col-2");
+  hourDiv.text(i + ":00");
+  timeBlock.append(hourDiv);
+
+//   create a div inside the timeblock for the input form with a space of col-10
+  var inputDiv = $("<div>");
+  inputDiv.addClass("textarea col-10");
+  timeBlock.append(inputDiv);
+
+
+
 }
+}
+
+
+createTimeBlocks();
